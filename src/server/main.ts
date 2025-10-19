0e218ffeffca3b73ff67b780755a399831d70bcd
BlackGepard777 <atomkagames6@gmail.com>
2025-10-19 14:13:19 +0300

    add database

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,5 +1,6 @@
 import express, { type NextFunction, type Request, type Response } from "express";
 import ViteExpress from "vite-express";
+import { initDb } from "./db";
 
 const app = express();
 
@@ -7,6 +8,7 @@ app.use(express.json());
 
 async function startServer() {
   try {
+    await initDb();
     ViteExpress.listen(app, 3001, () =>
       console.log("Server is listening on http://localhost:3001/")
     );
