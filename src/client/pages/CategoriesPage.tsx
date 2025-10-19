76bd409fa4ca36c4f2762572cabdc843ffb41256
BlackGepard777 <atomkagames6@gmail.com>
2025-10-19 15:01:11 +0300

    add style

--- src/client/pages/CategoriesPage.tsx
+++ src/client/pages/CategoriesPage.tsx
@@ -4,8 +4,7 @@ function CategoriesPage() {
     return (
         <main>
             <div className="container">    
-                <p className="subtitle" data-i18n="welcome">Ласкаво просимо до віртуального музею українського орнаменту.</p>
-                <button className="big-btn" id="start-btn" data-i18n="start">Розпочати подорож</button>
+                <p className="subtitle" data-i18n="welcome">Категорії</p>
             </div>
         </main>
     );
